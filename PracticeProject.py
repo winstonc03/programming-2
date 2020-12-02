35ecdd45def4c1d9fca6ed3e656ed9acebdc8b7f
Winston Chieng <winston.chieng03@gmail.com>
2020-12-02 10:26:08 -0800

    Add n == 1 and n == 2 in fib alg

--- PracticeProject.py
+++ PracticeProject.py
@@ -3,7 +3,17 @@
 # Code to practice using Git
 
 def main():
-    pass
+    """"" Calculate the nth Fibonacci number"""
+    # 1, 1, 2, 3, 5, 8, ...
+
+    # Ask user which fib number they want
+    num = int (input("Which Fibonacci number do you want?"))
+
+    # start at n = 1 -> 1
+    #          n = 2 -> 1
+    #          n = 3 -> 2
+    if num == 1 or  num == 2:
+        print(f"The {num}st/nd Fibonacci number is 1")
 
 
 if __name__ == "__main__":
