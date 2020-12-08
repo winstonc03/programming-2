92d404f9eaa5a6e25f629062c60a26257e68a5b9
Winston Chieng <66758884+winstonc03@users.noreply.github.com>
2020-12-08 11:38:42 -0800

    Update quiz.py
    add individual var for each question

--- quiz.py
+++ quiz.py
@@ -2,45 +2,49 @@
 # Author: Winston
 # Date: December 4, 2020
 
-import time
 name = input("Welcome! What's your name?\n")
 score = 0
 
 print(f"Hey {name}, I have a short quiz for you, Good Luck!")
-time.sleep(2)
 
-if input("Finish the sentence:  Who lives in a ___ under  the seaaa\n").lower() == "pineapple":
+q1 = input("Finish the sentence:  Who lives in a ___ under  the seaaa\n").lower().strip("!., -")
+if q1 == "pineapple":
     score += 1
     print("Correct!")
 else:
     print("The correct answer was 'pineapple' :( ")
 
-if input("What's 15 times 12?\n") == "180":
+q2 = input("What's 15 times 12?\n").lower().strip("!., -").replace(" ","")
+if q2 == "180" or q2 == "onehundredeighty":
     score += 1
     print("Correct!")
 else:
     print("The correct answer was '180' :( ")
 
-if input("Who is the main character in the 'Mario' Franchise?\n").lower() == "mario":
+q3 = input("Who is the main character in the 'Mario' Franchise?\n").lower().strip("!., -")
+if q3 == "mario":
     score += 1
     print("Correct!")
 else:
-    print("The correct answer was 'mario' :( ")
+    print("Cmonnnnnn :( ")
 
-if input("True or False: Penguins can fly.\n").lower() == "true":
+q4 = input("True or False: Penguins can fly.\n").lower().strip("!., -")
+if q4 == "true" or q4 == "t":
     score += 1
     print("Correct!")
 else:
-    print("The correct answer was 'True' :( ")
+    print("Nah penguins do be flying 0.o ")
 
-if input("Last Question, did you like this quiz?\n").lower() == "yes":
+q5 = input("Last Question, did you like this quiz?\n").lower().strip("!., -")
+if q5 == "yes" or q5 == "yessir":
     score += 1
-    print("Correct!")
+    print("Awesome!")
 else:
-    print("The correct answer was 'yes' >:( ")
+    print("Aw :( ")
 
 percent = score/5 * 100
 
-print(f"That's all! You're final percentage was {percent}%, You're awesome {name}!")
-
-#.lower.strip
+if (score >= 3):
+    print(f"That's all! Your final percentage was {percent}%, You're awesome {name}!")
+else:
+    print(f"That's all! Your final percentage was {percent}%, Nice try {name}!")
